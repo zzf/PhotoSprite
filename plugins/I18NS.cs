5b380177681b61615488d74a6192433272b0fda0
zzf <tyxkzzf123456@126.com>
2021-07-16 17:47:31 +0800

    统一文本读取

--- plugins/I18NS.cs
+++ plugins/I18NS.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Xml;
 using PhotoSprite.Plugins;
 
-
+/// <summary>
+/// 统一文本读取
+/// </summary>
 namespace PhotoSprite.Plugins
 {
     public class I18NS
